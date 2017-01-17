218495971cd62b941ad79c7a2ec78cb49b5929b0
jeff buenting <jeffbuenting@gmail.com>
2017-01-16 20:54:52 -0500

    Begin Get-WebAppPool

---
+++ Dev/Get-WebAppPool.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-WebAppPool" {
+    It "Output Application Pool Object" {
+        Get-WebAppPool | Should BeofType PSCustomObject
+    }
+}
