f7d807445bb5c65ceba54d1f4f497767bf484da8
jeffbuenting <jeffbuenting@gmail.com>
2018-01-10 21:26:49 -0500

    pester initialized

---
+++ internetexplorer.Tests.ps1
@@ -0,0 +1,44 @@
+﻿$ModulePath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+$ModuleName = $ModulePath | Split-Path -Leaf
+
+# ----- Remove and then import the module.  This is so any new changes are imported.
+Get-Module -Name $ModuleName -All | Remove-Module -Force -Verbose
+
+Import-Module "$ModulePath\$ModuleName.PSD1" -Force -ErrorAction Stop -Scope Global -Verbose
+
+#-------------------------------------------------------------------------------------
+
+Write-Output "`n`n"
+
+Describe "InternetExplorer : Get-IEWebVideo" {
+    Context "Help" {
+        $H = Help Get-IEWebVideo -Full
+        
+        # ----- Help Tests
+        It "has Synopsis Help Section" {
+            $H.Synopsis | Should Not BeNullorEmpty
+        }
+
+        It "has Description Help Section" {
+            $H.Description | Should Not BeNullorEmpty
+        }
+
+        It "has Parameters Help Section" {
+            $H.Parameters | Should Not BeNullorEmpty
+        }
+
+        # Examples - Remarks (small description that comes with the example)
+        foreach ($Example in $H.examples.example)
+        {
+            it "Example - Remarks on $($Example.Title)"{
+                $Example.remarks | Should not BeNullOrEmpty
+            }
+        }
+
+        It "has Notes Help Section" {
+            $H.alertSet | Should Not BeNullorEmpty
+        }
+    } 
+
+}
